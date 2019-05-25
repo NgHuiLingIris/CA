632bc1abc85ea1da13755035d8163bfeff2c10b5
Iris Ng <irisnghl@gmail.com>
2019-05-25 15:06:25 +0800

    Filtering

--- LAPS/src/main/java/com/example/portal/controller/HomepageController.java
+++ LAPS/src/main/java/com/example/portal/controller/HomepageController.java
@@ -1,10 +1,38 @@
 package com.example.portal.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.portal.model.Leave;
+import com.example.portal.model.User;
 
 @Controller
 public class HomepageController {
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("user", new User());
+        return "homepage";
+    }
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String loginView(User user,Model model) {
+		String employeedivision = user.getEmployeediv();
+		if (employeedivision.equals("Admin")){
+			return "homeAdmin";
+		}
+		else if (employeedivision.equals("Employee")){
+			return "homeEmployee";
+		}
+		else if (employeedivision.equals("Manager")){
+			return "homeManager";
+		}
+		else {
+			return "homepage";
+		}
+    }
 	@RequestMapping(path="/admin")
 	public String Admin()
 	{
